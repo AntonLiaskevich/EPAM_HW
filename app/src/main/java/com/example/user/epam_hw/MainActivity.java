3ecf9d865064a5e6dfdf00d0b7445a21d206a2b0
AntonLiaskevich <antlon.lesk@gmail.com>
2019-02-25 16:05:16 +0300

    merge

--- app/src/main/java/com/example/user/epam_hw/MainActivity.java
+++ app/src/main/java/com/example/user/epam_hw/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.user.epam_hw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "first feature2", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "second feature2", Toast.LENGTH_LONG).show();
     }
 }
