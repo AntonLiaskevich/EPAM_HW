406b9387262a3629804221e68666c1262f73cb67
AntonLiaskevich <antlon.lesk@gmail.com>
2019-03-25 23:41:53 +0300

    Navigation Drawer + Compound view

--- app/src/main/java/com/example/user/epam_hw/MainActivity.java
+++ app/src/main/java/com/example/user/epam_hw/MainActivity.java
@@ -1,23 +1,61 @@
 package com.example.user.epam_hw;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirstFragment firstFragment = new FirstFragment();
+    private SecondFragment secondFragment = new SecondFragment();
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "first feature2", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "second feature2", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "first feature1", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "second feature1", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(), "third feature1", Toast.LENGTH_LONG).show();
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_face_black_24dp);
 
-        Toast.makeText(getApplicationContext(), "third feature2", Toast.LENGTH_LONG).show();
-
-        Toast.makeText(getApplicationContext(), "using stash", Toast.LENGTH_LONG).show();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(navigationListener);
     }
+
+    private void setActiveFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener navigationListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem menuItem) {
+            drawerLayout.closeDrawers();
+
+            switch (menuItem.getItemId()) {
+                case R.id.first_fragment:
+                    setActiveFragment(firstFragment);
+
+                    break;
+                case R.id.second_fragment:
+                    setActiveFragment(secondFragment);
+
+                    break;
+            }
+
+            return true;
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/user/epam_hw/ProfileView.java
@@ -0,0 +1,47 @@
+package com.example.user.epam_hw;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
+public class ProfileView extends RelativeLayout {
+
+    private ImageView profileImage;
+
+    public ProfileView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ProfileView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ProfileView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.view_profile, this);
+        setGravity(Gravity.BOTTOM);
+        profileImage = findViewById(R.id.face_image);
+        profileImage.setOnClickListener(image);
+    }
+
+    OnClickListener image = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Random rand = new Random();
+            profileImage.setColorFilter(Color.rgb(rand.nextInt(254) + 1, rand.nextInt(254) + 1, rand.nextInt(254) + 1));
+        }
+    };
+}
+
