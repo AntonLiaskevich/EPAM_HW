0667ce8ec0e825bfe980c0ffdfe7be00e0256bd7
AntonLiaskevich <antlon.lesk@gmail.com>
2019-04-01 16:45:30 +0300

    add fragments

--- app/src/main/java/com/example/user/epam_hw/FirstFragment.java
+++ app/src/main/java/com/example/user/epam_hw/FirstFragment.java
@@ -12,6 +12,6 @@ public class FirstFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.first_fragment_layout, container, false);
     }
 }
--- app/src/main/java/com/example/user/epam_hw/SecondFragment.java
+++ app/src/main/java/com/example/user/epam_hw/SecondFragment.java
@@ -7,13 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 public class SecondFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.second_fragment_layout, container, false);
     }
 
 }
