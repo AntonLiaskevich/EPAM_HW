461c5bf9620add09a641ed247e8e5b4bbbcf90ae
AntonLiaskevich <antlon.lesk@gmail.com>
2019-04-25 00:21:43 +0300

    some changes

--- app/src/main/java/com/example/user/epam_hw/MainActivity.java
+++ app/src/main/java/com/example/user/epam_hw/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.user.epam_hw;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +11,6 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private FirstFragment firstFragment;
-    private SecondFragment secondFragment;
     private DrawerLayout drawerLayout;
 
     @Override
